1358cf7b0e27ac46e2f9270d9f30c65f52f93715
xNtrow <xntrowdev@gmail.com>
2022-05-18 21:12:09 +0200

    Updated [+] Settings

---
+++ src/main/java/io/github/sdxqw/module/settings/Settings.java
@@ -0,0 +1,9 @@
+package io.github.sdxqw.module.settings;
+
+import io.github.sdxqw.module.Module;
+
+public class Settings {
+    public String name;
+    public boolean shouldRender;
+    public Module module;
+}
